6eeda804940e38a927166effcb5a94ca0f6e888f
Vignesh1-art <vigneshshetty838@gmail.com>
2020-04-16 08:56:06 +0530

    Code made Understandable

--- central_value_therom.py
+++ central_value_therom.py
@@ -3,8 +3,9 @@ import numpy as np
 import matplotlib.pyplot as h
 mean_of_samples=[]
 no_of_samples=1000
+sample_size=8
 for i in range(no_of_samples):
-    sample=rn.randint(0,10,10)
+    sample=rn.randint(0,10,sample_size)
     mean_of_samples.append(np.mean(sample))
 h.hist(mean_of_samples)
 h.show()
